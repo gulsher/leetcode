eaae005bb03060f4defd599d32948e636de40a26
Gulsher <ahmed.gulsher@gmail.com>
2023-09-02 19:47:16 +0530

    Time: 54 ms (57.50%) | Memory: 41.8 MB (73.21%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    return Math.min(...nums)
+    let  left = 0;
+    let right = nums.length -1;
+
+    while(left < right){
+        let midPoint = Math.floor((left+right)/2);
+        if(nums[midPoint]>nums[right]){
+            left = midPoint + 1
+        }
+        else{
+            right = midPoint;
+        }
+    }
+    return nums[left]
 };
