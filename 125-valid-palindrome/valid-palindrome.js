899859db0997921c2d98c255cf72667aef625901
Gulsher <ahmed.gulsher@gmail.com>
2023-09-08 19:29:45 +0530

    Time: 60 ms (79.31%) | Memory: 44.1 MB (85.55%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+ let newStr = s.toLowerCase().replace(/[^0-9a-z]/g, "");
+    let left = 0, right = newStr.length-1;
+
+    while(left<right){
+        if(newStr[left] === newStr[right]){
+            left++;
+            right--;
+        }else{
+            return false
+        }
+    }
+    return true
+};
