770b2ba8ee8a811dd547af0e06e4184419d22e3f
Gulsher <ahmed.gulsher@gmail.com>
2023-09-29 10:53:06 +0530

    Time: 73 ms (68.12%) | Memory: 47 MB (61.61%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,18 +3,29 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    if(!s.length) return 0;
-    let left =0 ,right =0, set = new Set(), maxLength = -Infinity;
+    if (!s.length) return 0;
+  
+  let left = 0, right = 0;
+  let maxLength = -Infinity;
+  const set = new Set();
 
-    while(right< s.length){
-        if(!set.has(s[right])){
-            set.add(s[right]);
-            right ++;
-            maxLength = Math.max(maxLength,set.size) 
-        }else{
-            set.delete(s[left]);
-            left++
-        }
+  while (right < s.length) {
+    // If s[right] has not been seen yet
+    if (!set.has(s[right])) {
+      // Add it to the set
+      set.add(s[right]);
+      // Increase size of window to right
+      right++;
+      // Update maxLength; set size represents length of unique substring
+      maxLength = Math.max(maxLength, set.size);
+    } else {
+      // We've seen s[right] so we need to shrink the window
+      // Delete s[left] from set
+      set.delete(s[left]);
+      // Shrink window from left
+      left++;
     }
-    return maxLength
+  }
+
+  return maxLength;
 };
