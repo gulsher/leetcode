0023026702cc9723537ede4cc953b35c3423928e
Gulsher <ahmed.gulsher@gmail.com>
2023-10-03 17:57:50 +0530

    Time: 54 ms (69.26%) | Memory: 43.1 MB (16.49%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let p1 = 0;
+    let p2 = numbers.length - 1;
+
+    while (p1 < p2) {
+        let sum = numbers[p1] + numbers[p2];
+        if (sum === target) {
+            return [p1 + 1, p2 + 1];
+        } else if (sum > target) {
+            p2--;
+        } else {
+            p1++;
+        }
+    }
+};
