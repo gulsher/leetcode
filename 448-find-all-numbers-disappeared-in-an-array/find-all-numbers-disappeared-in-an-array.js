f2f8c0952b7985648243a982cbf8254219d2beb5
Gulsher <ahmed.gulsher@gmail.com>
2024-05-18 23:44:33 +0530

    Time: 99 ms (25.04%) | Memory: 70.1 MB (5.04%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    
+   const map = new Map();
+  nums.forEach((num, i) => map.set(num, i));
+
+  const result = [];
+  for (let i = 1; i <= nums.length; i++) {
+    if (!map.has(i)) result.push(i);
+  }
+  return result;
+
+};
+
