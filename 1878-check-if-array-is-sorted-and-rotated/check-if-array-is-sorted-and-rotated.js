e9338297f29021d548d0ec229372df20c30cc2e6
Gulsher <ahmed.gulsher@gmail.com>
2024-07-08 18:41:08 +0530

    Time: 47 ms (85.55%) | Memory: 49.3 MB (12.14%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+  let count = 0;
+    const len = nums.length - 1;
+    for(let i = 0; i < len; i++){
+        if(nums[i] > nums[i+1]) count++;
+    }
+    if(count > 1 || (count == 1 && nums[0] < nums[len])) return false;
+    return true;  
+};
