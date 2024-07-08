946f3d9494adc1807d4b52ab309808201413b6c0
Gulsher <ahmed.gulsher@gmail.com>
2024-07-08 18:20:17 +0530

    Time: 64 ms (78.27%) | Memory: 51.2 MB (34.23%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+    let count = 0;
+    
+    const bubbleFromCenter = (left, right) => {
+        while(left >= 0 && right < s.length && s[left] === s[right]) {
+            count++; left--; right++;
+        }
+    }
+    
+    for(let i = 0; i < s.length; i++) {
+        bubbleFromCenter(i, i);
+        bubbleFromCenter(i, i+1);
+    }
+    return count;
+};
